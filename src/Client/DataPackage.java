2352a89a6efc8020c316813466b3deb58f1c1259
Robert Ellis <robmadeyou@gmail.com>
2013-02-20 01:01:33 +0000

    Networing and shizz

---
+++ src/Client/DataPackage.java
@@ -0,0 +1,19 @@
+package Client;
+import java.io.Serializable;
+
+/**
+ * 
+ * @author Vallentin <vallentinsource@gmail.com>
+ * @since April 8, 2012
+ * 
+ */
+
+public class DataPackage implements Serializable
+{
+	public int  x = 0;
+	public int y = 0;
+	
+	public String username = "";
+	
+	
+}
---
+++ src/Server/DataPackage.java
@@ -0,0 +1,20 @@
+package Server;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @author Vallentin <vallentinsource@gmail.com>
+ * @since April 8, 2012
+ * 
+ */
+
+public class DataPackage implements Serializable
+{
+	public int x = 0;
+	public int y = 0;
+	
+	public String username = "";
+	
+	
+}
