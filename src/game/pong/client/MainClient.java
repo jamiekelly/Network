b9ad08e12252edd00a0e96461f3991cfb6e6e56d
Tim <timblack94@gmail.com>
2013-02-25 11:46:56 -0800

    Changed quite a bit (READ description)
    Deleted uneeded stuff, added (at least attempted to) pause
    functionality.  Didn't manage to test it because something went wrong
    with the game colors.  You need to look at the colors of everything and
    make sure everything is good because when I deleted the random colors
    everything screwed up.  Other than that the pause functionality needs
    testing.  I'm not sure if I implemented the sending of the isPaused
    variable correctly or not so check that too.

--- src/game/pong/client/MainClient.java
+++ src/game/pong/client/MainClient.java
@@ -1,7 +1,5 @@
 package game.pong.client;
 
-import game.pong.Server.Player;
-
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -9,8 +7,6 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Random;
-
 import javax.swing.JOptionPane;
 
 import org.lwjgl.LWJGLException;
@@ -42,13 +38,9 @@ import static org.lwjgl.opengl.GL11.glVertex2i;
 
 import static org.lwjgl.opengl.GL11.GL_QUADS;
 public class MainClient {
-	/*
-	//TODO we need to make sure the second player get's IDed as player 1
-	 * after that we need to make sure the second player controls ONLY the left paddle
-	 * I just want to raise these concerns if you haven't already thought about them
-	 * We should concentrate on making the server serve the ball once two clients have joined.
-	 * */
+
 	static boolean applet = false;
+	static boolean isPaused = false;
 	
 	static int playerNum = 0;
 	
@@ -151,69 +143,71 @@ public class MainClient {
 		try {
 			socket = new Socket(ip, port);
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 		ObjectInputStream ois;
 		try {
-			ois = new ObjectInputStream(socket.getInputStream());
-			JOptionPane.showMessageDialog(null, (String) ois.readObject());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
+				ois = new ObjectInputStream(socket.getInputStream());
+				JOptionPane.showMessageDialog(null, (String) ois.readObject());
+		} 
+		catch (Exception e) 
+		{
 			e.printStackTrace();
 		}
 		new Thread(receive).start();
 		new Thread(send).start();
 		}
-		float r,g,b;
-		Random random = new Random();
 		while(!Display.isCloseRequested())
 		{
-			r = random.nextFloat();
-			g = random.nextFloat();
-			b = random.nextFloat();
 			ball.x = ballX;
 			ball.y = ballY;
 			glClear(GL_COLOR_BUFFER_BIT);
 			
 			/* Controls */
-			if(Keyboard.isKeyDown(Keyboard.KEY_UP)) //Player paddle
+			if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
 			{
-				if(y > 0)
-				{
-					if(playerNum == 0){
-						y -= 5;
-					}else{
-						p2Y -= 5;
-					}
-					//TODO Update server about location
+				if(isPaused==false){
+					isPaused=true;
+				}
+				if(isPaused==true){
+					isPaused=false;
 				}
 			}
-			if(Keyboard.isKeyDown(Keyboard.KEY_DOWN))
+			if(isPaused==false)
 			{
-				if(y + 60 < Display.getHeight())
+				if(Keyboard.isKeyDown(Keyboard.KEY_UP)) //Player paddle
 				{
-					if(playerNum == 0){
-						y += 5;
-					}else{
-						p2Y += 5;
+					if(y > 0)
+					{
+						if(playerNum == 0){
+							y -= 5;
+						}else{
+							p2Y -= 5;
+						}
+						
 					}
-					//TODO Update server about location
+				}
+				if(Keyboard.isKeyDown(Keyboard.KEY_DOWN))
+				{
+					if(y + 60 < Display.getHeight())
+					{
+						if(playerNum == 0){
+							y += 5;
+						}else{
+							p2Y += 5;
+						}
+					}
 				}
 			}
-			
 			
 			/*Text at the top of the screen*/
 			Fonts.drawString("Pong",(Display.getWidth()/2)-50,20, 1);
 			Fonts.drawString(score0 + "", (int)(Display.getWidth()*.25)-40, 20, 1);
 			Fonts.drawString(score1 + "", (int)(Display.getWidth()*.75)-40, 20, 1);
-			
-			/* <addition space> */
-			
-			
 			
 			/*
 			 * 1.......2
@@ -222,15 +216,14 @@ public class MainClient {
 			 * .       .
 			 * 4.......3
 			 */
-			
-			glColor3f(r,g,b);
-			glBegin(GL_QUADS);  // Player 0 paddle, Left paddle
+			glBegin(GL_QUADS);  // Display window color
 			glVertex2i(0 , 0);	//1
 				glVertex2i(Display.getWidth(), 0);	//2
 				glVertex2i(Display.getWidth(), Display.getHeight());	//3
 				glVertex2i(0 , Display.getHeight());	//4
 			glEnd();
 			glColor3f(1,1,1);
+			
 			glBegin(GL_QUADS);  // Player 0 paddle, Left paddle
 				glVertex2i(x , y);	//1
 				glVertex2i(x + 20 , y);	//2
@@ -297,6 +290,7 @@ public class MainClient {
 					if(playerNum == 0){
 						ois = new ObjectInputStream(socket.getInputStream());
 						p2Y = (Integer) ois.readObject();
+						isPaused = (Boolean) ois.readObject();
 					}
 					if(playerNum == 1){
 						ois = new ObjectInputStream(socket.getInputStream());
@@ -310,12 +304,13 @@ public class MainClient {
 						score0 = (Integer) ois.readObject();
 						ois = new ObjectInputStream(socket.getInputStream());
 						score1 = (Integer) ois.readObject();
+						isPaused = (Boolean) ois.readObject();
 					}
 				Thread.sleep(10);
-			}catch(Exception e){
-				e.printStackTrace();
+				}catch(Exception e){
+					e.printStackTrace();
+				}
 			}
-		}
 		}
 	};
 	private static Runnable send = new Runnable()
@@ -338,6 +333,9 @@ public class MainClient {
 							
 							oos = new ObjectOutputStream(socket.getOutputStream());
 							oos.writeObject(score1);
+							
+							oos = new ObjectOutputStream(socket.getOutputStream());
+							oos.writeObject(isPaused);
 						}
 						if(playerNum == 1){
 							oos.writeObject(p2Y);
@@ -350,76 +348,81 @@ public class MainClient {
 	private static Runnable onUpdate = new Runnable(){
 		public void run() 
 		{
-			
 			while(true){
-				if(true){
-					if(ball.dX > 0){
-						ball.dX += 0.2;
-					}else{
-						ball.dX -= 0.2;
+				if(isPaused==false){
+					if(true){
+						if(ball.dX > 0){
+							ball.dX += 0.2;
+						}else{
+							ball.dX -= 0.2;
+						}
+						ballX -= ball.dX;
+						ballY -= ball.dY;
+						
+						int bX = ballX + 10;
+						int bY = ballY + 10;
+						System.out.println(bX + "  :  " + bY);
+						//Checking collision for the two player paddles if the
+						//ball is colliding with the paddle
+						boolean one = bX >= x && bX <= x + 20 && bY >= y && bY <= y + 60;
+						boolean two = bX >= p2X && bX <= p2X + 20 && bY >= p2Y && bY <= p2Y + 60;
+						if(one){
+							ball.dX = -ball.dX;
+							//Calculating where the ball will go after being hit off
+							//the paddle, same as in brick breaker
+							ball.dY = ((y + (20 / 2)) - (bY + 10)) / 10;
+						}
+						if(two){
+							ball.dX = -ball.dX;
+							//Calculating where the ball will go after being hit off
+							//the paddle, same as in brick breaker
+							ball.dY = ((p2Y + (20 / 2)) - (bY + 10)) / 10;
+						}
+						if(ballX > Display.getWidth()){
+							score0 ++;
+							ball.dX = 5;
+							ball.dY = 0;
+							ballX = Display.getWidth() / 2;
+							ballY = Display.getHeight() / 2;
+						}
+						if(ballX < 0){
+							score1 ++;
+							ball.dX = 5;
+							ball.dY = 0;
+							ballX = Display.getWidth() / 2;
+							ballY = Display.getHeight() / 2;
+						}
+						if(ballY < 0){
+							ballY = 1;
+							ball.dY = -ball.dY;
+						}
+						if(ballY > Display.getHeight()){
+							ballY = Display.getHeight() - 1;
+							ball.dY = -ball.dY;
+						}
+							/*
+							 * That's actually a good idea, how about a count down
+							 * like when the ball is reset then it will spawn back in
+							 * like 2 seconds or something? or the player who scored
+							 * gets to start it?
+							 * -Rob
+							 * 
+							 * 
+							 * IT'S A VERY GOOD IDEA!!!!
+							 * DO THAT!!
+							 * -Tim
+							 */
+							//TODO ^^^^^^^
+						try {
+							Thread.sleep(20);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
 					}
-					ballX -= ball.dX;
-					ballY -= ball.dY;
-					
-					int bX = ballX + 10;
-					int bY = ballY + 10;
-					System.out.println(bX + "  :  " + bY);
-					int pW = 20;
-					int pH = 60;
-					//Checking collision for the two player paddles if the
-					//ball is colliding with the paddle
-					boolean one = bX >= x && bX <= x + 20 && bY >= y && bY <= y + 60;
-					boolean two = bX >= p2X && bX <= p2X + 20 && bY >= p2Y && bY <= p2Y + 60;
-					if(one){
-						ball.dX = -ball.dX;
-						//Calculating where the ball will go after being hit off
-						//the paddle, same as in brick breaker
-						ball.dY = ((y + (20 / 2)) - (bY + 10)) / 10;
-					}
-					if(two){
-						ball.dX = -ball.dX;
-						//Calculating where the ball will go after being hit off
-						//the paddle, same as in brick breaker
-						ball.dY = ((p2Y + (20 / 2)) - (bY + 10)) / 10;
-					}
-					if(ballX > Display.getWidth()){
-						score0 ++;
-						ball.dX = 5;
-						ball.dY = 0;
-						ballX = Display.getWidth() / 2;
-						ballY = Display.getHeight() / 2;
-					}
-					if(ballX < 0){
-						score1 ++;
-						ball.dX = 5;
-						ball.dY = 0;
-						ballX = Display.getWidth() / 2;
-						ballY = Display.getHeight() / 2;
-					}
-					if(ballY < 0){
-						ballY = 1;
-						ball.dY = -ball.dY;
-					}
-					if(ballY > Display.getHeight()){
-						ballY = Display.getHeight() - 1;
-						ball.dY = -ball.dY;
-					}
-						/*
-						 * That's actually a good idea, how about a count down
-						 * like when the ball is reset then it will spawn back in
-						 * like 2 seconds or something? or the player who scored
-						 * gets to start it?
-						 * -Rob
-						 */
-					try {
-						Thread.sleep(20);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					}
+				}else{
+					Fonts.drawString("Paused" + "", (int)(Display.getWidth()/2)-40, Display.getHeight()/2, 1);
 				}
 			}
-		
+		}
 	};
 }
